1e7d13c97c71baf61f13183d6ab263c62bc8b69f
Alexey6262 <med3edev2006@yandex.com>
2025-12-06 19:31:20 +0300

    Show detailed user history with pagination

--- telegram_bot/app/states/search_state.py
+++ telegram_bot/app/states/search_state.py
@@ -3,3 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 class SearchState(StatesGroup):
     waiting_for_query = State()
     waiting_for_selection = State()
+    waiting_for_review = State()
+    waiting_for_watched_at = State()
+    waiting_for_rating = State()
