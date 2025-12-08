9583d130a8c389dbcbcbf6906800ad2aadb19c49
Alexey6262 <med3edev2006@yandex.com>
2025-12-08 15:16:47 +0300

    Fix watchlist rating response type

--- api/app/utils/dependencies.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.database import get_db
-
-# Простая зависимость для получения сессии БД
-get_db_session = get_db
